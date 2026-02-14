522fadab61163e3b61733db913b8405821a8dbb2
Kate Mendoza <16334@coderacademy.edu.au>
2026-02-14 23:03:34 +1100

    fix env variables

--- src/apiEndpoints/apiClient.js
+++ src/apiEndpoints/apiClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://13.211.58.52:3000/';
+const BEARER_TOKEN = import.meta.env.TOKEN
 
 // Create an axios instance
 const apiClient = axios.create({
@@ -14,7 +15,7 @@ apiClient.interceptors.request.use(config => {
   // const token = localStorage.getItem("token");
   // console.log("token", token)
   // if (token) {
-    config.headers.Authorization = `Bearer CoderAcademyCharliInnaKate`;
+    config.headers.Authorization = `Bearer ${BEARER_TOKEN}`;
   // }
   return config;
 })
